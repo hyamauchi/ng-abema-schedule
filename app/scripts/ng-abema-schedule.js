a17004351d9e086130e169df571ede10f2947a14
Hayato Yamauchi <h.yamauchi@outlook.com>
2016-12-25 11:06:06 +0900

    ngRouteを導入して「次の日をみる」ボタンを実装

--- app/scripts/ng-abema-schedule.js
+++ app/scripts/ng-abema-schedule.js
@@ -2,6 +2,7 @@ var ngAbemaSchedule = (function() {
   return {
     getUrl: function(yyyyMMdd) {
       var url = 'http://localhost:63342/ng-abema-schedule/testdata/examples.json';
+      // url = 'http://localhost:63342/ng-abema-schedule/testdata/' + yyyyMMdd + '.json';
       return url;
     }
   }
